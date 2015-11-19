728118a26c0b2f01e3c847b24ff130fbba3c95bc
tammyta <tammyta96@gmail.com>
2015-11-19 01:38:35 +0000

    update parser with class

--- bot/parser.js
+++ bot/parser.js
@@ -1,7 +1,7 @@
-"user strict"
+"use strict"
 
-var Parser = function Parser(data) {
-  
+class Parser {
+  constructor (data) {
     this.line = data
     this.space = 0;
     this.position = 0;
@@ -15,82 +15,77 @@ var Parser = function Parser(data) {
     }
     
     this.message.raw = this.line;
-    
-};
-
-Parser.prototype.parse = function parse() {
+  }
   
+  parse() {
     this._trimWhitespace();
     this._parsePrefix();
     this._trimWhitespace();
     this._parseCommand();
     this._trimWhitespace();
     this._parseParams();
-
+  
     return this.message;
-};
-
-Parser.prototype._trimWhitespace = function trimWhitespace() {
-    while (this.line.charCodeAt(this.position) === 32) {
+  }
+  _trimWhitespace() {
+    while (this.line[this.position] === ' ') {
         this.position++;
     }
-};
+  }
 
-Parser.prototype._parsePrefix = function parsePrefix() {
-    if (this.line.charCodeAt(this.position) === 58) {
-        this.space = this.line.indexOf(" ", this.position);
-        if (this.space === -1) { return null; }
+  _parsePrefix() {
+      if (this.line[this.position] === ':') {
+          this.space = this.line.indexOf(' ', this.position);
+          if (this.space === -1) { return null; }
+  
+          this.message.prefix = this.line.slice(this.position + 1, this.space).replace(':', '');
+          this.position = this.space + 1;
+      }
+  }
 
-        this.message.prefix = this.line.slice(this.position + 1, this.space).replace(':', '');
-        this.position = this.space + 1;
-    }
-};
+  _parseCommand() {
+      this.space = this.line.indexOf(' ', this.position);
+  
+      if (this.space === -1) {
+          if (this.line.length > this.position) {
+              this.message.command = this.line.slice(this.position);
+          }
+          return;
+      }
+  
+      this.message.command = String(this.line.slice(this.position, this.space));
+      this.position = this.space + 1;
+  }
 
-Parser.prototype._parseCommand = function parseCommand() {
-    this.space = this.line.indexOf(" ", this.position);
-
-    if (this.space === -1) {
-        if (this.line.length > this.position) {
-            this.message.command = this.line.slice(this.position);
-        }
-        return;
-    }
-
-    this.message.command = String(this.line.slice(this.position, this.space));
-    this.position = this.space + 1;
-};
-
-Parser.prototype._parseParams = function parseParams() {
-    while (this.position < this.line.length) {
-
-        this.space = this.line.indexOf(" ", this.position);
-
-        // Trailing!
-        if (this.line.charCodeAt(this.position) === 58) {
-            this.message.trailing = this.line.slice(this.position + 1);
-            break;
-        }
-
-        // Keep looping!
-        if (this.space !== -1) {
-            this.message.params.push(this.line.slice(this.position, this.space));
-            this.position = this.space + 1;
-            
-            while (this.line.charCodeAt(this.position) === 32) {
-                this.position++;
-            }
-            continue;
-        }
-
-        // We hit the end.
-        if (this.space === -1) {
-            this.message.params.push(this.line.slice(this.position));
-            
-            break;
-        }
-    }
-
-};
+  _parseParams() {
+      while (this.position < this.line.length) {
+  
+          this.space = this.line.indexOf(' ', this.position);
+  
+          // Trailing!
+          if (this.line[this.position] === ':') {
+              this.message.trailing = this.line.slice(this.position + 1);
+              break;
+          }
+  
+          // Keep looping!
+          if (this.space !== -1) {
+              this.message.params.push(this.line.slice(this.position, this.space));
+              this.position = this.space + 1;
+              
+              this._trimWhitespace();
+              continue;
+          }
+  
+          // We hit the end.
+          if (this.space === -1) {
+              this.message.params.push(this.line.slice(this.position));
+              
+              break;
+          }
+      }
+  
+  }
+}
 
 module.exports = Parser;
-
